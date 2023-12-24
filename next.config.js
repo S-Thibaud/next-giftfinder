ad0cc7f92772ecc0d94db1cfe831a3bdaab1d1d5
S-Thibaud <92119856+S-Thibaud@users.noreply.github.com>
2023-12-24 19:30:50 +0100

    removed StyledComponents from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  styledComponents: {
-    // Your styled-components configuration options go here
-  },
   basePath: '/github-pages',
 }
 
