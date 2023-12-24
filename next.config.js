8511edc9e90e94925a83aa1073cd24730459ae31
S-Thibaud <92119856+S-Thibaud@users.noreply.github.com>
2023-12-24 21:38:15 +0100

    site fixed

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
-// next.config.js
-const path = require('path');
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/github-pages',
-  webpack: (config, { isServer }) => {
-    // Additional Webpack configurations can be added here
-
-    // Example: Add a custom alias for imports
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-    };
-
-    // Example: Add a rule to handle SVG files
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    // Add other Webpack configurations as needed
-
-    return config;
+  compiler: {
+    styledComponents: true
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
