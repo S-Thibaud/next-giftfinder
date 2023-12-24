2724d532877916bf096ad595e6430425ae11e712
S-Thibaud <92119856+S-Thibaud@users.noreply.github.com>
2023-12-24 20:15:46 +0100

    removed db.js file

--- src/pages/categories/db.js
+++ src/pages/categories/db.js
@@ -1,23 +1,23 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'next-giftfinder',
-});
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'next-giftfinder',
+// });
 
-module.exports = {
-  query: (text, values) => {
-    return new Promise((resolve, reject) => {
-      pool.query(text, values, (err, results) => {
-        if (err) {
-          console.error('Database error:', err); // Log the error for debugging
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-  },
-};
+// module.exports = {
+//   query: (text, values) => {
+//     return new Promise((resolve, reject) => {
+//       pool.query(text, values, (err, results) => {
+//         if (err) {
+//           console.error('Database error:', err); // Log the error for debugging
+//           reject(err);
+//         } else {
+//           resolve(results);
+//         }
+//       });
+//     });
+//   },
+// };
